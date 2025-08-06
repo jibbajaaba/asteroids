68b6100355ae888de1ca6aea8460fa8b850a94a7
Mark A McTague <mmctaguecoding@gmail.com>
2025-08-05 20:32:18 -0400

    added comments to code

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from constants import *
 def main():
     # Initialize pygame
     pygame.init()
+    # set game screen height and width
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Game loop
     while True:
+        # allows player to exit out of game loop
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        # creates black background
         screen.fill("black")
         pygame.display.flip()
 
