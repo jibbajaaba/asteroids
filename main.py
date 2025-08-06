e1894f70a4886b32f77b3e29067b3701f2d1fca3
Mark A McTague <mmctaguecoding@gmail.com>
2025-08-05 20:45:03 -0400

    create clock object to set FPS to 60

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main():
     pygame.init()
     # set game screen height and width
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # create clock object and save to variable
+    clock = pygame.time.Clock()
+    # dt = delta time
+    dt = 0
 
     # Game loop
     while True:
@@ -13,10 +17,14 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
         # creates black background
         screen.fill("black")
         pygame.display.flip()
+        # set clock object to 60 milliseconds and divide by 1000 to convert to seconds
+        # save to dt
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
